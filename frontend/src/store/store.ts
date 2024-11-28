cf4ead8038f670475b837051fcd24ff7b8daef7a
trantrunghieu <deathclick2309@gmail.com>
2024-11-28 14:34:22 +0700

    updated: added reducers to redux store & export types of state & dispatch

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { authApi } from '@/services/authApi';
+import authReducer from './auth/authSlice';
 import themeReducer from './theme/themeSlice';
+import settingReducer from './setting/settingSlice';
 
 const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     theme: themeReducer,
+    setting: settingReducer,
+    auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
