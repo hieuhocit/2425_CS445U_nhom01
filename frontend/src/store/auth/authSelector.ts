a56a9a87f195658744476ca5fa52f5894df38a32
trantrunghieu <deathclick2309@gmail.com>
2024-11-29 23:32:34 +0700

    updated: added selector for isAdmin

--- frontend/src/store/auth/authSelector.ts
+++ frontend/src/store/auth/authSelector.ts
@@ -5,5 +5,9 @@ const selectAuth = (state: RootState) => state.auth;
 
 const userSelector = createSelector(selectAuth, (auth) => auth.user);
 const loginSelector = createSelector(selectAuth, (auth) => auth.isLoggedIn);
+const isAdminSelector = createSelector(
+  selectAuth,
+  (auth) => auth.user?.isAdmin
+);
 
-export { selectAuth, userSelector, loginSelector };
+export { selectAuth, userSelector, loginSelector, isAdminSelector };
