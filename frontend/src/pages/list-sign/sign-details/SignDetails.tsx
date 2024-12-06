e105868d286a1aa24958ae8bd4d4c3883036d670
trantrunghieu <deathclick2309@gmail.com>
2024-12-06 15:23:04 +0700

    update: change static data to dynamic data

--- frontend/src/pages/list-sign/sign-details/SignDetails.tsx
+++ frontend/src/pages/list-sign/sign-details/SignDetails.tsx
@@ -8,9 +8,20 @@ import Header from '@/components/header/Header';
 import { themeMode } from '@/store/theme/themeSelector';
 import { useSelector } from 'react-redux';
 
+/** react-router */
+import { useParams } from 'react-router-dom';
+
+/** DUMMY DATA */
+import { signs, signTopics } from '@/data/data';
+
 export default function SignDetailsPage() {
   const mode = useSelector(themeMode);
   const isDarkMode = mode === 'dark';
+
+  const { signId } = useParams();
+
+  const signTopic = signTopics.find((st) => st.id === Number(signId));
+  const list = signs.filter((s) => s.sign_topic_id === signTopic?.id);
 
   return (
     <div
@@ -18,101 +29,25 @@ export default function SignDetailsPage() {
         isDarkMode ? styles.darkMode : undefined
       }`}
     >
-      <Header title='Biển báo cấm' isDark={isDarkMode} />
+      <Header title={signTopic?.display as string} isDark={isDarkMode} />
       <main className={styles.main}>
         <ul className={styles.list}>
-          <li className={styles.item}>
-            <div className={styles.head}>
-              <h2>P.101: Đường cấm</h2>
-            </div>
-            <div className={styles.body}>
-              <img
-                src='https://beta.gplx.app/images/signs/signP101.png'
-                alt='sign image'
-              />
-              <p>
-                Biển báo đường cấm tất cả các loại phương tiện tham gia giao
-                thông đi lại cả hai hướng, trừ xe ưu tiên theo luật định.
-              </p>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <div className={styles.head}>
-              <h2>P.102: Cấm đi ngược chiều</h2>
-            </div>
-            <div className={styles.body}>
-              <img
-                src='https://beta.gplx.app/images/signs/signP102.png'
-                alt='sign image'
-              />
-              <p>
-                Biển báo đường cấm tất cả các loại phương tiện tham gia giao
-                thông đi vào theo chiều đặt biển.
-              </p>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <div className={styles.head}>
-              <h2>P.103a: Cấm ô tô</h2>
-            </div>
-            <div className={styles.body}>
-              <img
-                src='https://beta.gplx.app/images/signs/signP103a.png'
-                alt='sign image'
-              />
-              <p>
-                Biển báo đường cấm tất cả các loại xe cơ giới kể cả mô tô 3 bánh
-                có thùng đi qua, trừ xe mô tô 2 bánh, xe gắn máy (kể cả xe máy
-                điện) và các xe được ưu tiên theo Luật Giao thông đường bộ.
-              </p>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <div className={styles.head}>
-              <h2>P.103b: Cấm ô tô rẽ phải</h2>
-            </div>
-            <div className={styles.body}>
-              <img
-                src='https://beta.gplx.app/images/signs/signP103b.png'
-                alt='sign image'
-              />
-              {/* <p>
-                Biển báo đường cấm xe ô tô rẽ phải ( kể cả xe mô tô ba bánh),
-                trừ các xe được ưu tiên theo Luật Giao thông đường bộ.
-              </p> */}
-            </div>
-          </li>
-          <li className={styles.item}>
-            <div className={styles.head}>
-              <h2>P.103a: Cấm ô tô</h2>
-            </div>
-            <div className={styles.body}>
-              <img
-                src='https://beta.gplx.app/images/signs/signP103a.png'
-                alt='sign image'
-              />
-              <p>
-                Biển báo đường cấm tất cả các loại xe cơ giới kể cả mô tô 3 bánh
-                có thùng đi qua, trừ xe mô tô 2 bánh, xe gắn máy (kể cả xe máy
-                điện) và các xe được ưu tiên theo Luật Giao thông đường bộ.
-              </p>
-            </div>
-          </li>
-          <li className={styles.item}>
-            <div className={styles.head}>
-              <h2>P.103b: Cấm ô tô rẽ phải</h2>
-            </div>
-            <div className={styles.body}>
-              <img
-                src='https://beta.gplx.app/images/signs/signP103b.png'
-                alt='sign image'
-              />
-              {/* <p>
-                Biển báo đường cấm xe ô tô rẽ phải ( kể cả xe mô tô ba bánh),
-                trừ các xe được ưu tiên theo Luật Giao thông đường bộ.
-              </p> */}
-            </div>
-          </li>
+          {list.map((s) => (
+            <li className={styles.item}>
+              <div className={styles.head}>
+                <h2>
+                  {s.code}: {s.name}
+                </h2>
+              </div>
+              <div className={styles.body}>
+                <img
+                  src={`https://beta.gplx.app/images/signs/sign${s.image}.png`}
+                  alt={s.name + ' sign image'}
+                />
+                <p>{s.detail}</p>
+              </div>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
