1927a32ac0a1571d1aae7186dc51f32fdf1ac499
trantrunghieu <deathclick2309@gmail.com>
2024-12-11 13:30:45 +0700

    update: (make it better) use loader instead of useEffect & redux store

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,52 +1,62 @@
 // ** React router
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 /** toastify */
 import { Bounce, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 /** react-redux */
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { themeMode } from './store/theme/themeSelector';
 import { useGetUserQuery } from './services/authApi';
-import { currentLicenseSelector } from './store/setting/settingSelector';
-import { changeQuestionsAndExams } from './store/data/dataSlice';
+import { currentLicenseIdSelector } from './store/setting/settingSelector';
 
 /** API */
 import { getExams } from './services/examApi';
 import { getQuestions } from './services/questionApi';
 import { useGetLicensesQuery } from './services/licenseApi';
+import { getTopics } from './services/topicApi';
 
 /** react */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+/** store */
+import store from './store/store';
+
+export async function loader() {
+  const licenseId = store.getState().setting.currentLicenseId || 1;
+
+  const resQuestions = await getQuestions(licenseId);
+  const resExams = await getExams(licenseId);
+  const resTopics = await getTopics(licenseId);
+
+  return {
+    topics: resTopics?.data,
+    exams: resExams?.data,
+    questions: resQuestions?.data?.questions,
+  };
+}
 
 export default function Layout() {
+  const [firstRun, setFirstRun] = useState(true);
+
   const mode = useSelector(themeMode);
   const isDarkMode = mode === 'dark';
 
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useGetLicensesQuery();
+
   const { isLoading } = useGetUserQuery();
 
-  const licenseId = useSelector(currentLicenseSelector)?.id;
+  const licenseId = useSelector(currentLicenseIdSelector);
 
   useEffect(() => {
-    if (!licenseId) return;
-
-    async function setupData() {
-      const resQuestions = await getQuestions(licenseId);
-      const resExams = await getExams(licenseId);
-
-      dispatch(
-        changeQuestionsAndExams({
-          exams: resExams?.data,
-          questions: resQuestions?.data,
-        })
-      );
+    if (!licenseId || firstRun) {
+      setFirstRun(false);
+      return;
     }
-
-    setupData();
+    navigate('/', { replace: true });
   }, [licenseId]);
 
   return (
