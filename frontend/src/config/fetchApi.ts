fc845ff6d2c83ef46e0cdd482c3c4baf26bacb46
trantrunghieu <deathclick2309@gmail.com>
2024-12-12 12:49:55 +0700

    update: add postApi

--- frontend/src/config/fetchApi.ts
+++ frontend/src/config/fetchApi.ts
@@ -3,3 +3,13 @@ const apiUrl = import.meta.env.VITE_API_URL;
 export async function getApi(pathname: string) {
   return fetch(`${apiUrl}/${pathname}`);
 }
+
+export async function postApi(pathname: string, data: object) {
+  return fetch(`${apiUrl}/${pathname}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
