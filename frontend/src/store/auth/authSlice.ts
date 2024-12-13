a33aa9627bd2c7e97dd01a1a38f03be3ca8ac207
trantrunghieu <deathclick2309@gmail.com>
2024-12-13 11:47:07 +0700

    update: does not store in localStorage anymore

--- frontend/src/store/auth/authSlice.ts
+++ frontend/src/store/auth/authSlice.ts
@@ -7,8 +7,6 @@ const authSlice = createSlice({
   initialState: {
     user: null,
     isLoggedIn: false,
-    access_token: null,
-    refresh_token: null,
   } as AuthState,
   reducers: {},
   extraReducers: (builder) => {
@@ -16,34 +14,16 @@ const authSlice = createSlice({
       .addMatcher(authApi.endpoints.login.matchFulfilled, (state, action) => {
         const user = action.payload.data as User;
         state.user = user;
-
-        state.access_token = user.access_token;
-        state.refresh_token = user.refresh_token;
         state.isLoggedIn = true;
-
-        localStorage.setItem('access_token', user.access_token);
-        localStorage.setItem('refresh_token', user.refresh_token);
       })
       .addMatcher(authApi.endpoints.logout.matchFulfilled, (state) => {
         state.user = null;
-
-        state.access_token = null;
-        state.refresh_token = null;
         state.isLoggedIn = false;
-
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('refresh_token');
       })
       .addMatcher(authApi.endpoints.getUser.matchFulfilled, (state, action) => {
         const user = action.payload.data as User;
         state.user = user;
-
-        state.access_token = user.access_token;
-        state.refresh_token = user.refresh_token;
         state.isLoggedIn = true;
-
-        localStorage.setItem('access_token', user.access_token);
-        localStorage.setItem('refresh_token', user.refresh_token);
       });
   },
 });
