38dcd7a513b5e1e9c837db44e3366a9781304ccf
trantrunghieu <deathclick2309@gmail.com>
2024-12-15 17:03:04 +0700

    update: handle more situations

--- frontend/src/pages/exam-result/ExamResult.tsx
+++ frontend/src/pages/exam-result/ExamResult.tsx
@@ -11,10 +11,30 @@ import Header from '@/components/header/Header';
 import QuestionDetails from '@/components/question-details/QuestionDetails';
 
 /** types */
-import { IAnswer, IQuestion } from '@/types/definitions';
+import { IAnswer, IExam, ILicense, IQuestion } from '@/types/definitions';
 
 /** react-router */
 import { useLocation, useNavigate } from 'react-router-dom';
+
+/** react-router */
+import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
+
+/** API */
+import { getQuestions } from '@/services/historyApi';
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const { examHistoryId } = params;
+
+  if (!examHistoryId) return null;
+
+  const res = await getQuestions(examHistoryId as string);
+  const resData = await res.json();
+
+  return {
+    exam: resData?.data?.exam,
+    questions: resData?.data?.questions,
+  };
+}
 
 /** icons */
 import {
@@ -32,19 +52,70 @@ export default function ExamResultPage() {
 
   const currentLicense = useSelector(currentLicenseSelector);
 
-  const { questions }: { questions: IQuestion[] } = location.state;
+  const mode = useSelector(themeMode);
+  const isDarkMode = mode === 'dark';
 
-  const totalTrueAnswer = questions.filter((q) => {
+  let prevPath;
+  let title;
+  let questions;
+
+  const loaderData = useLoaderData() as {
+    exam: IExam | undefined | null;
+    questions: IQuestion[] | undefined | null;
+  };
+
+  console.log(loaderData, location?.state);
+
+  if (location?.state) {
+    const state = location.state as {
+      questions: IQuestion[];
+      title: string;
+      prevPath: string;
+    };
+    prevPath = state?.prevPath;
+    title = state?.title;
+    questions = state?.questions;
+  } else {
+    prevPath = '..';
+    title = `Lịch sử thi ${
+      Number(loaderData?.exam?.title) ? `đề số ${loaderData?.exam?.title}` : ''
+    } `;
+    questions = loaderData?.questions;
+  }
+
+  if (!questions || !prevPath || !title) {
+    return (
+      <div
+        className={`${styles.examResult} ${
+          isDarkMode ? styles.darkMode : undefined
+        }`}
+      >
+        <Header title={'Đã xảy ra lỗi'} isDark={isDarkMode} path={'..'} />
+        <main className={styles.main}>
+          <div className={styles.body}>
+            {!questions && (
+              <p>
+                Đã xảy ra lỗi, vui lòng thử lại sau hoặc liên hệ với quản trị
+                viên.
+              </p>
+            )}
+          </div>
+        </main>
+      </div>
+    );
+  }
+
+  const totalTrueAnswer = questions?.filter((q) => {
     const trueAnswer = (q.answers as IAnswer[]).find((a) => a.correct);
     return q.idSelectedAnswer === trueAnswer?.id;
   }).length;
 
-  const totalFalseAnswer = questions.filter((q) => {
+  const totalFalseAnswer = questions?.filter((q) => {
     const trueAnswer = (q.answers as IAnswer[]).find((a) => a.correct);
     return q.idSelectedAnswer && q.idSelectedAnswer !== trueAnswer?.id;
   }).length;
 
-  const totalRequiredAnswerTrue = questions.filter((q) => {
+  const totalRequiredAnswerTrue = questions?.filter((q) => {
     if (q.required) {
       const trueAnswer = (q.answers as IAnswer[]).find((a) => a.correct);
       return q.idSelectedAnswer === trueAnswer?.id;
@@ -52,17 +123,14 @@ export default function ExamResultPage() {
     return false;
   }).length;
 
-  const totalSkipAnswer = questions.filter((q) => {
+  const totalSkipAnswer = questions?.filter((q) => {
     return !q.idSelectedAnswer;
   }).length;
 
-  const totalQuestion = questions.length;
+  const totalQuestion = questions?.length;
 
   const isWrongRequiredAnswer =
-    totalRequiredAnswerTrue !== questions.filter((q) => q.required).length;
-
-  const mode = useSelector(themeMode);
-  const isDarkMode = mode === 'dark';
+    totalRequiredAnswerTrue !== questions?.filter((q) => q.required).length;
 
   return (
     <div
@@ -70,7 +138,7 @@ export default function ExamResultPage() {
         isDarkMode ? styles.darkMode : undefined
       }`}
     >
-      <Header title='Kết quả bài thi' isDark={isDarkMode} path='/list-exam' />
+      <Header title={title} isDark={isDarkMode} path={prevPath} />
       <main className={styles.main}>
         <div className={styles.head}>
           {questions && questions.length > 0 && (
@@ -120,13 +188,13 @@ export default function ExamResultPage() {
                   <span
                     className={`${styles.result} ${
                       isWrongRequiredAnswer &&
-                      totalTrueAnswer >= currentLicense.pass
+                      totalTrueAnswer >= (currentLicense as ILicense)?.pass
                         ? styles.colorGreen
                         : styles.colorRed
                     }`}
                   >
                     {isWrongRequiredAnswer &&
-                    totalTrueAnswer >= currentLicense.pass
+                    totalTrueAnswer >= (currentLicense as ILicense)?.pass
                       ? 'ĐẠT'
                       : 'CHƯA ĐẠT'}
                   </span>
@@ -168,7 +236,7 @@ export default function ExamResultPage() {
       <footer className={styles.footer}>
         {questions && questions.length > 0 && (
           <div className={styles.actions}>
-            <button onClick={() => navigate('/list-exam')}>Quay lại</button>
+            <button onClick={() => navigate(prevPath)}>Quay lại</button>
           </div>
         )}
       </footer>
