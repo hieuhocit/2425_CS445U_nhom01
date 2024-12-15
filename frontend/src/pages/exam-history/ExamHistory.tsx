a21ae15b870a488affea70dd784807d1b953915b
trantrunghieu <deathclick2309@gmail.com>
2024-12-15 17:06:54 +0700

    update: integrate api with real data

--- frontend/src/pages/exam-history/ExamHistory.tsx
+++ frontend/src/pages/exam-history/ExamHistory.tsx
@@ -6,14 +6,36 @@ import { useSelector } from 'react-redux';
 import { themeMode } from '@/store/theme/themeSelector';
 
 /** react-router */
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 /** components */
 import Header from '@/components/header/Header';
 
+/** API */
+import store from '@/store/store';
+
+/** API */
+import { getExamHistory } from '@/services/historyApi';
+
+/** types */
+import { IExam } from '@/types/definitions';
+
+export async function loader() {
+  const licenseId =
+    store.getState().setting.currentLicenseId ||
+    localStorage.getItem('licenseId') ||
+    1;
+
+  const res = await getExamHistory(licenseId);
+  const resData = await res.json();
+  return resData?.data;
+}
+
 export default function ExamHistoryPage() {
   const mode = useSelector(themeMode);
   const isDarkMode = mode === 'dark';
+
+  const exams = useLoaderData() as IExam[] | undefined | null;
 
   return (
     <>
@@ -21,18 +43,22 @@ export default function ExamHistoryPage() {
         <Header title='Lịch sử thi' isDark={isDarkMode} />
 
         <main className={styles.main}>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <Link to='/exam-history/examId1'>
-                <h2>Đề số 01</h2>
-              </Link>
-            </li>
-            <li className={styles.item}>
-              <Link to='/exam-history/examId2'>
-                <h2>Đề số 02</h2>
-              </Link>
-            </li>
-          </ul>
+          {exams && exams.length > 0 && (
+            <ul className={styles.list}>
+              {exams.map((e) => (
+                <li key={e.id} className={styles.item}>
+                  <Link to={`/exam-history/${e.id}`}>
+                    <h2>{Number(e.title) ? `Đề số ${e.title}` : e.title}</h2>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+          {exams && exams.length === 0 && (
+            <p className={styles.message}>
+              Hiện tại bạn chưa làm bài thi nào cho giấy phép này
+            </p>
+          )}
         </main>
       </div>
     </>
