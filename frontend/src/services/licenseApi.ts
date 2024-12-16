e30cfe9ff04ac13d33cfc4735384a178f6c1e27e
trantrunghieu <deathclick2309@gmail.com>
2024-12-16 13:17:17 +0700

    update: attach cookie when fetch

--- frontend/src/services/licenseApi.ts
+++ frontend/src/services/licenseApi.ts
@@ -11,6 +11,7 @@ export const licenseApi = createApi({
   reducerPath: 'licenseApi',
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
+    credentials: 'include',
   }),
 
   endpoints: (builder) => ({
